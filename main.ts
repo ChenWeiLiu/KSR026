4de8a8c5bc78111d60aaffb12c9a49a4731c193f
white liu <a500667337@gmail.com>
2018-09-11 13:49:27 +0800

    設定led 燈光顏色

--- main.ts
+++ main.ts
@@ -5,13 +5,20 @@
 namespace KSR026 {
     /**
     * 顯示HC-SR04
-    * 
-    * 
+    *  設定為公分
     */
     //% block
     export function sr04(): number {
         return sonar.ping(DigitalPin.P13, DigitalPin.P14, PingUnit.Centimeters)
     }
-
+    /**
+    * TODO: 在此描述函式
+    * @param value 在此描述值, eg: 5
+    */
+    //% block
+    export function led(color: NeoPixelColors) {
+        const use_led = neopixel.create(DigitalPin.P16, 2, NeoPixelMode.RGB);
+        use_led.showColor(neopixel.colors(color))
+    }
    
 }
