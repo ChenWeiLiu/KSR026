27943ee041c46c4affa5f70ac37d7daad3327edb
white liu <a500667337@gmail.com>
2018-09-11 13:58:52 +0800

    add 中文化

--- main.ts
+++ main.ts
@@ -7,15 +7,15 @@ namespace KSR026 {
     * 顯示HC-SR04
     *  設定為公分
     */
-    //% block
+    //% blockId="sr04" block="sr04"
     export function sr04(): number {
         return sonar.ping(DigitalPin.P13, DigitalPin.P14, PingUnit.Centimeters)
     }
     /**
-    * TODO: 在此描述函式
+    * 顯示 LED
     * @param value 在此描述值, eg: 5
     */
-    //% block
+    //% blockId="led" block="led %RGB"
     export function led(color: NeoPixelColors) {
         const use_led = neopixel.create(DigitalPin.P16, 2, NeoPixelMode.RGB);
         use_led.showColor(neopixel.colors(color))
